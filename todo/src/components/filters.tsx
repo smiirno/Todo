9a4a7bdead2ddd00b3e8f6f1fe8153037868f722
smiirno <Arseny.Smirnov@urfu.me>
2022-10-31 21:28:14 +0500

    add active filters

--- todo/src/components/filters.tsx
+++ todo/src/components/filters.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './filters.css'
 import {Link} from "react-router-dom";
 import {useAppDispatch} from "../store/redux";
@@ -8,16 +8,47 @@ const Filters = () => {
 
     const dispatch = useAppDispatch()
 
-    const clickHandler = () => {
+    const [active, setActive] = useState([]);
+
+    document.getElementById('id')
+
+    const clickHandler = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+        const linkAll = document.getElementById('all')
+        const linkActive = document.getElementById('active')
+        const linkCompleted = document.getElementById('completed')
+        const currentLink = document.getElementById(event.currentTarget.id)
+        if (currentLink && linkAll && linkActive && linkCompleted) {
+            switch (currentLink) {
+                case linkAll:
+                    currentLink.classList.add('active')
+                    linkActive.classList.remove('active')
+                    linkCompleted.classList.remove('active')
+                    break;
+                case linkActive:
+                    currentLink.classList.add('active')
+                    linkAll.classList.remove('active')
+                    linkCompleted.classList.remove('active')
+                    break;
+                case linkCompleted:
+                    currentLink.classList.add('active')
+                    linkActive.classList.remove('active')
+                    linkAll.classList.remove('active')
+            }
+        }
+    }
+
+    const removeHandler = () => {
         dispatch(removeCompletedTodos())
     }
 
     return (
         <div className={'container filters'}>
-            <Link to={''} className={'filter'}>All</Link>
-            <Link to={'/active'} className={'filter'}>Active</Link>
-            <Link to={'/completed'} className={'filter'}>Completed</Link>
-            <button className={'filter'} onClick={clickHandler}>Clear completed</button>
+            <div>
+                <Link to={''} className={'filter active'} id={'all'} onClick={event => clickHandler(event)}>All</Link>
+                <Link to={'/active'} className={'filter'} id={'active'} onClick={event => clickHandler(event)}>Active</Link>
+                <Link to={'/completed'} className={'filter'} id={'completed'} onClick={event => clickHandler(event)}>Completed</Link>
+            </div>
+            <button className={'filter'} onClick={removeHandler}>Clear completed</button>
         </div>
     );
 };
